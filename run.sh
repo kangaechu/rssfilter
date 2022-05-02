1d01c6a153b83754350fb92a3842b520c5607b48
kangaechu <kangae2@gmail.com>
2022-05-02 15:49:45 +0900

    change name from learn to train

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# import
-rssfilter import -u "https://b.hatena.ne.jp/hotentry/it.rss" -f hot-it.json
+# fetch
+rssfilter fetch -u "https://b.hatena.ne.jp/hotentry/it.rss" -f hot-it.json
 
 # classify
 rssfilter classify -f hot-it.json -m hot-it.model
