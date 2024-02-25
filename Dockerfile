df6cbd9234577d47cbed1212b38868a3ab50f24b
isucon <isucon@example.com>
2024-02-25 16:24:40 +0900

    Doker環境を追加

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+FROM busybox:1.36-uclibc as busybox
+
+FROM rclone/rclone:1 as rclone
+
+FROM golang:1.22-bookworm AS build
+
+WORKDIR /app
+COPY go.mod go.sum ./
+RUN go mod download
+COPY main.go ./
+COPY rssfilter/ ./rssfilter
+COPY cmd/ ./cmd
+
+RUN go build -o /app/main -ldflags '-s -w' main.go
+
+
+FROM gcr.io/distroless/base-debian12
+
+COPY --from=busybox /bin/busybox /bin/busybox
+RUN ["/bin/busybox", "--install", "/bin"]
+
+USER nonroot:nonroot
+WORKDIR /app
+COPY --chown=nonroot:nonroot --from=build /app/main /app/rssfilter
+COPY --chown=nonroot:nonroot --from=rclone /usr/local/bin/rclone /usr/local/bin/rclone
+COPY --chown=nonroot:nonroot run.sh /app/run.sh
+
+# ENV SYNC_CLOUD_STORAGE="true"
+# ENV RSS_URL
+# ENV RCLONE_DESTINATION
+# ENV RCLONE_CONFIG
+
+ENTRYPOINT ["/bin/sh", "-c", "/app/run.sh"]
--- run.sh
+++ run.sh
@@ -1,8 +1,35 @@
-#!/usr/bin/env bash
+#!/bin/sh
+
+set -eou pipefail
+
+JSON_FILE="data/hot-it.json"
+MODEL_FILE="data/hot-it.model"
+RSS_FILE="public/hot-it.xml"
+
+cd "$(dirname "$0")"
+
+if [ ! -f "${JSON_FILE}" ]; then echo "error: ${JSON_FILE} not found"; exit 1; fi
+if [ ! -f "${MODEL_FILE}" ]; then echo "error: ${MODEL_FILE} not found"; exit 1; fi
+if [ -z "${SYNC_CLOUD_STORAGE}" ]; then echo "error: SYNC_CLOUD_STORAGE is not set"; exit 1; fi
+if [ "${SYNC_CLOUD_STORAGE}" ]; then
+  if [ -z "${RSS_URL}" ]; then echo "error: RSS_URL is not set"; exit 1; fi
+  if [ -z "${RCLONE_DESTINATION}" ]; then echo "error: RCLONE_DESTINATION is not set"; exit 1; fi
+  if [ -z "${RCLONE_CONFIG}" ]; then echo "error: RCLONE_CONFIG is not set"; exit 1; fi
+fi
 
 # fetch
-rssfilter fetch -u "https://b.hatena.ne.jp/hotentry/it.rss" -f hot-it.json
+./rssfilter fetch -u "https://b.hatena.ne.jp/hotentry/it.rss" -f "${JSON_FILE}"
 
 # classify
-rssfilter classify -f hot-it.json -m hot-it.model
+./rssfilter classify -f "${JSON_FILE}" -m "${MODEL_FILE}"
+
+if [ -n "${SYNC_CLOUD_STORAGE}" ]; then
+  # setup rclone
+  echo "$RCLONE_CONFIG" | base64 -d > rclone.conf
+  # generate RSS
+  ./rssfilter export -f "${JSON_FILE}" -r "${RSS_FILE}" -u "${RSS_URL}"
+
+  # upload
+  /usr/local/bin/rclone -v sync --config rclone.conf "${RSS_FILE}" "${RCLONE_DESTINATION}"
+fi
 
